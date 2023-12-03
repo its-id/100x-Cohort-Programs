f1aa689d5557e8a6f727334914d8933dc2176bb2
its-id <ik.dana24@gmail.com>
2023-12-03 20:00:33 +0530

    Complete For loop

---
+++ Week 1/main.js
@@ -0,0 +1,16 @@
+// console.log('hello world');
+// console.log(a);
+
+// let firstName = 'indrakant';
+// let age = 23;
+// let isMarried = true;
+
+// if (isMarried) console.log(firstName + 'is married');
+// if (!isMarried) console.log(firstName + 'is not married');
+
+let answer = 0;
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+  answer += i;
+}
+console.log('sum: ' + answer);
