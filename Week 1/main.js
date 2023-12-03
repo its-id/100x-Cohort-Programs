557c78d69a9c80cfc2bd2af8c2aa89a4e20c2bec
its-id <ik.dana24@gmail.com>
2023-12-03 20:09:07 +0530

    complete arrays and loops

--- Week 1/main.js
+++ Week 1/main.js
@@ -1,16 +1,43 @@
-// console.log('hello world');
-// console.log(a);
+// basic syntax
+/*
+console.log('hello world');
+console.log(a);
+*/
 
-// let firstName = 'indrakant';
-// let age = 23;
-// let isMarried = true;
+// if else
+/*
+let firstName = 'indrakant';
+let age = 23;
+let isMarried = true;
 
-// if (isMarried) console.log(firstName + 'is married');
-// if (!isMarried) console.log(firstName + 'is not married');
+if (isMarried) console.log(firstName + 'is married');
+if (!isMarried) console.log(firstName + 'is not married');
+*/
 
+// loops
+/*
 let answer = 0;
 for (let i = 1; i <= 10; i++) {
   console.log(i);
   answer += i;
 }
 console.log('sum: ' + answer);
+*/
+
+
+// arrays
+/*
+const personArray = ['harkirat', 'raman', 'indrakant'];
+*/
+
+//write a program to print even ages
+/*
+const ages = [21, 22, 23, 24, 25];
+for(let i = 0; i < ages.length; i++){
+    if(ages[i]%2 == 0){
+        console.log(ages[i]);
+    }
+}
+*/
+
+
