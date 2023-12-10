d40dc2d6a6d783a81cb7bea8a36bdb91fc53d2ee
its-id <ik.dana24@gmail.com>
2023-12-11 00:58:16 +0530

    complete 1-file-cleaner

---
+++ Week 2/Assignments/medium/1-file-cleaner/1-file-cleaner.js
@@ -0,0 +1,28 @@
+// ## File cleaner
+// Read a file, remove all the extra spaces and write it back to the same file.
+
+// For example, if the file input was
+// ```
+// hello     world    my    name   is       raman
+// ```
+
+// After the program runs, the output should be
+
+// ```
+// hello world my name is raman
+// ```
+
+const fs = require('fs');
+
+fs.readFile('a.txt', 'utf-8', function (err, data) {
+  let str = data.replace(/\s+/g, ' ');
+  fs.writeFile('a.txt', str, function (err) {
+    if (err) console.log('err: ', err);
+    else {
+      console.log('File written successfully!');
+      console.log(str);
+    }
+  });
+});
+
+console.log('outside async readFile');
