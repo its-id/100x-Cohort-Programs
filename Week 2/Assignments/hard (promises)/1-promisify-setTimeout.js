4ec8fab4dd05129f98bf69dcacaa98e5e7b427dd
its-id <ik.dana24@gmail.com>
2023-12-11 00:39:19 +0530

    complete 1-promisify-setTimeout

--- Week 2/Assignments/hard (promises)/1-promisify-setTimeout.js
+++ Week 2/Assignments/hard (promises)/1-promisify-setTimeout.js
@@ -3,4 +3,16 @@
 */
 
 function wait(n) {
+    return new Promise(function(resolve, reject){
+        setTimeout(function(){
+            resolve('promise resolved after ' + n + ' seconds');   
+        }, n*1000);
+    })
 }
+
+const p = wait(1);
+p.then(function(data){
+    console.log(data);
+    console.log('inside .then() function after promise is resolved');
+})
+
