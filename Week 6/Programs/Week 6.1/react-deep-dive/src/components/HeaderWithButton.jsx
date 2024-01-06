0c370862b84cdaf932f7450c568843bb635beee9
its-id <ik.dana24@gmail.com>
2024-01-06 19:50:52 +0530

    shift state updation logic to new component

---
+++ Week 6/Programs/Week 6.1/react-deep-dive/src/components/HeaderWithButton.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const HeaderWithButton = () => {
+  const [title, setTitle] = useState('React Deep Dive');
+
+  const changeTitle = () => {
+    // Math.random gives a random number b/w 0 and 1
+    // toString(36) converts the number to a string in base 36
+    // substring(7) gets the first 7 characters of the string
+    const newTitle = Math.random().toString(36).substring(7);
+    setTitle(newTitle);
+  };
+
+  return <p>{title}</p>;
+};
+
+export default HeaderWithButton;
