9e7fa8920eb5315f65c8720ef034e4cc151126fd
its-id <ik.dana24@gmail.com>
2024-01-06 19:53:35 +0530

    using react.memo()

--- Week 6/Programs/Week 6.1/react-deep-dive/src/App.jsx
+++ Week 6/Programs/Week 6.1/react-deep-dive/src/App.jsx
@@ -4,12 +4,20 @@ import Header from './components/Header';
 import HeaderWithButton from './components/HeaderWithButton';
 
 function App() {
+  const [title, setTitle] = useState('React Deep Dive');
+
+  const changeTitle = () => {
+    // Math.random gives a random number b/w 0 and 1
+    // toString(36) converts the number to a string in base 36
+    // substring(7) gets the first 7 characters of the string
+    const newTitle = Math.random().toString(36).substring(7);
+    setTitle(newTitle);
+  };
 
   return (
     //below is what we call a fragment
     <>
-      
-      <HeaderWithButton />
+      <Header title={title} onClick={changeTitle} />
       <Header title='React Deep Dive' />
     </>
   );
--- Week 6/Programs/Week 6.1/react-deep-dive/src/components/Header.jsx
+++ Week 6/Programs/Week 6.1/react-deep-dive/src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Header = (props) => {
+//react will only re-render this component if the props change
+const Header = React.memo((props) => {
   return (
     <p>{props.title}</p>
   )
-}
+});
 
 export default Header
