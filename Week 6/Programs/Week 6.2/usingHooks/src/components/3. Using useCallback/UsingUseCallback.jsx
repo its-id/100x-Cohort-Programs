0c0a76ff0366479518d1556333c53352662d047f
its-id <ik.dana24@gmail.com>
2024-01-07 21:21:46 +0530

    Update UsingUseCallback.jsx

--- Week 6/Programs/Week 6.2/usingHooks/src/components/3. Using useCallback/UsingUseCallback.jsx
+++ Week 6/Programs/Week 6.2/usingHooks/src/components/3. Using useCallback/UsingUseCallback.jsx
@@ -3,14 +3,14 @@ import { useState, memo, useCallback } from 'react';
 const UsingUseCallback = () => {
   const [count, setCount] = useState(0);
 
-  //whenever we click on the child component, the child component re-renders, even though the child is wrapper under memo() and the prop function is not changing.
+  //whenever we click on the child component, the child component re-renders, even though the child is wrapped under memo() and the prop "function" is not changing.
   //   const onClickHandler = () => {
   //     console.log('Button clicked');
   //   };
   //This is because the function is created again and again with different REFERENTIAL EQUALITY on every re-render. To prevent this, we use useCallback hook
   const onClickHandler = useCallback(() => {
     console.log('Parent function called!');
-  });
+  });//ensures that the function is created only once and the same function is used on every click
 
   return (
     <>
