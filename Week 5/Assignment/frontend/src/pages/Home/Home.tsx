e445bdfa869e57f6b57c1ac52e6902f38a33d83a
its-id <ik.dana24@gmail.com>
2024-01-12 03:41:56 +0530

    update cards page ui

--- Week 5/Assignment/frontend/src/pages/Home/Home.tsx
+++ Week 5/Assignment/frontend/src/pages/Home/Home.tsx
@@ -8,8 +8,20 @@ import Card from '../../components/Card/Card';
 const Home = () => {
   const [cards, setCards] = useState([]);
   const [role, setRole] = useState('user');
-
+  const [socials, setSocials]: any = useState([]);
   const navigate = useNavigate();
+
+  const addSocialInput = () => {
+    setSocials([...socials, { name: '', url: '' }]);
+  };
+
+  const handleSocialChange = (e: any, idx: number) => {
+    const { name, value } = e.target;
+    const list = [...socials];
+    list[idx][name] = value;
+    setSocials(list);
+  };
+
   const getCards = async () => {
     try {
       const response = await axios.get('http://localhost:3001/cards', {
@@ -34,9 +46,67 @@ const Home = () => {
 
   return (
     <div className={styles.container}>
-      {cards.map((card: any) => (
-        <Card {...card} role={role} />
-      ))}
+      <h2 className={styles.header}>Cards</h2>
+      <div className={styles.sectionWrapper}>
+        <div className={styles.addCard}>
+          <input
+            className={styles.addCard__input}
+            type='text'
+            placeholder='name'
+          />
+          <input
+            className={styles.addCard__input}
+            type='text'
+            placeholder='description'
+          />
+          <input
+            className={styles.addCard__input}
+            type='text'
+            placeholder='interests. eg: games, coding'
+          />
+          <div className={styles.socialsInputHeader}>
+            <p>Socials</p>
+            <button onClick={addSocialInput}>Add</button>
+          </div>
+
+          <div className={styles.socialsInputContainer}>
+            {socials.map((social: any, idx: number) => (
+              <div key={`social-${idx}`}>
+                <input
+                  className={styles.socialsInput__input}
+                  type='text'
+                  name='name'
+                  placeholder='name'
+                  value={social.name}
+                  onChange={(e) => handleSocialChange(e, idx)}
+                />
+                <span>:</span>
+                <input
+                  className={styles.socialsInput__input}
+                  type='text'
+                  name='url'
+                  placeholder='url'
+                  value={social.url}
+                  onChange={(e) => handleSocialChange(e, idx)}
+                />
+              </div>
+            ))}
+          </div>
+          <button
+            className={styles.addCardBtn}
+            onClick={() => {
+              navigate('/add');
+            }}
+          >
+            Add Card
+          </button>
+        </div>
+        <div className={styles.cardsSection}>
+          {cards.map((card: any, idx: number) => (
+            <Card key={idx} {...card} role={role} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
