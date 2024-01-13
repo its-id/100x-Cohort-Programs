e0df0d7d4fd77a394f8e342185038bd47a87000e
its-id <ik.dana24@gmail.com>
2024-01-13 19:25:30 +0530

    create week 7.1 project and setup routes

---
+++ Week 7/Programs/Week 7.1/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Dashboard from './components/Dashboard';
+import Landing from './components/Landing';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
---
+++ Week 7/Programs/Week 7.1/src/components/Landing.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <div>Landing</div>
+  )
+}
+
+export default Landing
