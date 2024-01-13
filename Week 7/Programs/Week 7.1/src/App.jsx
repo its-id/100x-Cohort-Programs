111d3d121b8131f1e50469ad3f51b91012daa882
its-id <ik.dana24@gmail.com>
2024-01-13 19:37:48 +0530

    redirecting using window.location.href

--- Week 7/Programs/Week 7.1/src/App.jsx
+++ Week 7/Programs/Week 7.1/src/App.jsx
@@ -1,15 +1,29 @@
-import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Dashboard from './components/Dashboard';
 import Landing from './components/Landing';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
-      <div>TopBar</div>
+      <div>
+        <button
+          onClick={() => {
+            window.location.href = '/'; //it is a global object in the browser, we simply change the 'href' key's value
+
+            //FLAW: all the html, css, js files are fetched again. similar to a reload.
+          }}
+        >
+          Landing
+        </button>
+        <button
+          onClick={() => {
+            window.location.href = '/dashboard';
+          }}
+        >
+          Dashboard
+        </button>
+      </div>
       <BrowserRouter>
         <Routes>
           <Route path='/dashboard' element={<Dashboard />} />
