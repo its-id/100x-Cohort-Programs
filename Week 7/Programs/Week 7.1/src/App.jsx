e26456ecfd9fbedf22ff61a570dad675c4f3f5c3
its-id <ik.dana24@gmail.com>
2024-01-13 19:43:56 +0530

    using useNavigate() hook

--- Week 7/Programs/Week 7.1/src/App.jsx
+++ Week 7/Programs/Week 7.1/src/App.jsx
@@ -1,30 +1,42 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
 import Dashboard from './components/Dashboard';
 import Landing from './components/Landing';
 
+const TopBar = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <button
+        onClick={() => {
+          // window.location.href = '/'; //it is a global object in the browser, we simply change the 'href' key's value
+
+          //FLAW: all the html, css, js files are fetched again. similar to a reload.
+
+          //to make sure it maintains the same bundle and not fetch again, we use useNavigate() hook, but we can only use it only in the context of a <Router> component.
+          navigate('/');
+        }}
+      >
+        Landing
+      </button>
+      <button
+        onClick={() => {
+          // window.location.href = '/dashboard';
+
+          navigate('/dashboard');
+        }}
+      >
+        Dashboard
+      </button>
+    </>
+  );
+};
+
 function App() {
   return (
     <div>
-      <div>
-        <button
-          onClick={() => {
-            window.location.href = '/'; //it is a global object in the browser, we simply change the 'href' key's value
-
-            //FLAW: all the html, css, js files are fetched again. similar to a reload.
-          }}
-        >
-          Landing
-        </button>
-        <button
-          onClick={() => {
-            window.location.href = '/dashboard';
-          }}
-        >
-          Dashboard
-        </button>
-      </div>
       <BrowserRouter>
+        <TopBar />
         <Routes>
           <Route path='/dashboard' element={<Dashboard />} />
           <Route path='/' element={<Landing />} />
