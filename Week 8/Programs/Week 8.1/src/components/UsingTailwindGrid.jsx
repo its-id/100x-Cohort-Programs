bfa43aec318d2081b194d40eeb50ee3dbdb295d9
its-id <ik.dana24@gmail.com>
2024-01-20 19:50:02 +0530

    making app responsive |

--- Week 8/Programs/Week 8.1/src/components/UsingTailwindGrid.jsx
+++ Week 8/Programs/Week 8.1/src/components/UsingTailwindGrid.jsx
@@ -1,6 +1,6 @@
 const UsingTailwindGrid = () => {
   return (
-    <div className='grid grid-cols-3'>
+    <div className='grid grid-cols-2 md:grid-cols-3'>
       <div className='bg-red-400 text-center'>Red Box</div>
       <div className='bg-green-400 text-center'>Green Box</div>
       <div className='bg-yellow-400 text-center'>Yellow Box</div>
