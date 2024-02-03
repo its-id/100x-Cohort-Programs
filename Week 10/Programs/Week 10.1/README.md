f0305808cad1c7f0f991f2be2a5ba96ec1d67452
its-id <ik.dana24@gmail.com>
2024-02-03 21:19:42 +0530

    complete steps till slide 11

--- Week 10/Programs/Week 10.1/README.md
+++ Week 10/Programs/Week 10.1/README.md
@@ -1,21 +1,30 @@
-## To get Database url
+### To get Database url online
 
 Create one at [Neon](https://neon.tech/)
 
-## Steps to run the database locally
+### Steps to run the database locally
 
 Step 1: Install docker [here](https://docs.docker.com/engine/install/)
 
-**Rest of the steps are to get the postgres database running\. (To be run in the terminal)** 👇
+**Steps are to get the postgres database running\. (To be run in the terminal)** 👇
 Step 2: Pull the latest PostgreSQL Docker: `docker pull postgres`
-Step 3: Verify if pulled down successfully: `docker images` 
-Step 4: Create docker volume: `docker volume create 100x-postgres` 
-Step 5: Verify if created successfully: `docker volume ls` 
-Step 6: Create PostgreSQL container (mac/linux specific, check the path for windows): `docker run --name 100x-postgres -e POSTGRES_PASSWORD=mysecretpassword -p 5432:5432 -v 100x-postgres:/var/lib/postgresql/data -d postgres` 
+Step 3: Verify if pulled down successfully: `docker images`
+Step 4: Create docker volume: `docker volume create 100x-postgres`
+Step 5: Verify if created successfully: `docker volume ls`
+Step 6: Create PostgreSQL container (mac/linux specific, check the path for windows): `docker run --name 100x-postgres -e POSTGRES_PASSWORD=mysecretpassword -p 5432:5432 -v 100x-postgres:/var/lib/postgresql/data -d postgres`
 Step 7: Access PostgreSQL using psql: `docker exec -it 100x-postgres psql -U postgres`
+
+<br>
+
+**Important Step**: Create a **.env** file in the root directory and add content like shown in .env.example file
 
 ---
 
-8. Create a **.env** file in the root directory and add content like shown in .env.example file
+### To run the steps.
+
+1. `npm install` (one time)
+2. Uncomment the /_ .. _/ part for each STEP in the `index.ts` file and run the following command in the terminal.
+3. `tsc -b`
+4. `node dist/index.js`
 
 > **Note**: If you are using windows, you might need to change the volume path to `C:\ProgramData\docker\volumes\100x-postgres\_data` or something similar.
--- Week 10/Programs/Week 10.1/src/index.ts
+++ Week 10/Programs/Week 10.1/src/index.ts
@@ -115,3 +115,153 @@ async function getUser(email: string) {
 // Example usage
 getUser('user5@example.com').catch(console.error);
 */
+
+
+// STEP 5: CREATING A FOREIGN TABLE AND CREATING RELATIONSHIP
+/*
+async function createAddressesTable(){
+    await client.connect();
+    const result = await client.query(`
+    CREATE TABLE addresses (
+        id SERIAL PRIMARY KEY,
+        user_id INTEGER NOT NULL,
+        city VARCHAR(100) NOT NULL,
+        country VARCHAR(100) NOT NULL,
+        street VARCHAR(255) NOT NULL,
+        pincode VARCHAR(20),
+        created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    )`);
+    console.log(result);
+}
+createAddressesTable();
+*/
+
+// STEP 6: INSERTING DATA INTO FOREIGN TABLE (WITHOUT USING TRANSACTION)
+/*
+async function insertDataIntoAddressesTable(user_id: number, city: string, country: string, street: string, pincode: string){
+    try {
+        await client.connect();
+        const insertQuery = 'INSERT INTO addresses (user_id, city, country, street, pincode) VALUES ($1, $2, $3, $4, $5)';
+        const values = [user_id, city, country, street, pincode];
+        const res = await client.query(insertQuery, values);
+        console.log('Insertion success:', res);
+    } catch (err) {
+        console.error('Error during the insertion:', err);
+    } finally {
+        await client.end();
+    }
+}
+
+// Example usage
+insertDataIntoAddressesTable(1, 'New York', 'USA', '1234, Street Name', '123').catch(console.error);
+*/
+
+
+
+
+
+
+
+
+// STEP 7: INSERTING DATA INTO FOREIGN TABLE (USING TRANSACTION)
+/*
+async function insertDataIntoAddressesTable(user_id: number, city: string, country: string, street: string, pincode: string){
+    try {
+        await client.connect();
+
+        // Begin the transaction
+        await client.query('BEGIN');
+        const insertQuery = 'INSERT INTO addresses (user_id, city, country, street, pincode) VALUES ($1, $2, $3, $4, $5)';
+        const values = [user_id, city, country, street, pincode];
+        const res = await client.query(insertQuery, values);
+        console.log('Insertion success:', res);
+
+        // Commit the transaction
+        await client.query('COMMIT');
+    } catch (err) {
+        await client.query('ROLLBACK');
+        console.error('Error during the insertion:', err);
+    } finally {
+        await client.end();
+    }
+}
+
+// Example usage
+insertDataIntoAddressesTable(1, 'New York', 'USA', '1234, Street Name', '123').catch(console.error);
+*/
+
+
+
+
+// STEP 8: GETTING USER WITH ADDRESS (WITHOUT JOIN) (NOT RECOMMENDED)
+/*
+async function getUserWithAddress(email: string) {
+    try {
+        await client.connect();
+        const query = 'SELECT * FROM users WHERE email = $1';
+        const values = [email];
+        const result = await client.query(query, values);
+    
+        if (result.rows.length > 0) {
+          console.log('User found:', result.rows[0]);
+          const user = result.rows[0];
+          const addressQuery = 'SELECT * FROM addresses WHERE user_id = $1';
+          const addressValues = [user.id];
+          const addressResult = await client.query(addressQuery, addressValues);
+          if (addressResult.rows.length > 0) {
+            console.log('Address found:', addressResult.rows[0]);
+            user.address = addressResult.rows[0];
+          } else {
+            console.log('No address found for the user.');
+          }
+          return user;
+        } else {
+          console.log('No user found with the given email.');
+          return null;
+        }
+      } catch (err) {
+        console.error('Error during fetching user:', err);
+        throw err;
+      } finally {
+        await client.end();
+      }
+}
+
+getUserWithAddress('user3@example.com');
+*/
+
+
+
+
+
+
+
+
+
+// STEP 8: GETTING USER WITH ADDRESS (WITH JOIN) (RECOMMENDED)
+/*
+async function getUserWithAddress(email: string) {
+    try {
+        await client.connect();
+        const query = 'SELECT * FROM users u JOIN addresses a ON u.id = a.user_id WHERE u.email = $1';
+        const values = [email];
+        const result = await client.query(query, values);
+    
+        if (result.rows.length > 0) {
+          console.log('User found:', result.rows[0]);
+          return result.rows[0];
+        } else {
+          console.log('No user found with the given email.');
+          return null;
+        }
+      } catch (err) {
+        console.error('Error during fetching user:', err);
+        throw err;
+      } finally {
+        await client.end();
+      }
+}
+
+getUserWithAddress('user3@example.com');
+*/
