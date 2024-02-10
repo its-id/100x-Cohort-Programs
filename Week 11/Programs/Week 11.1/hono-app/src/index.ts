f62e62c39f284dd6c05e0627bb8ab619eaf819f5
its-id <ik.dana24@gmail.com>
2024-02-10 20:54:29 +0530

    using get calls & middlewares

--- Week 11/Programs/Week 11.1/hono-app/src/index.ts
+++ Week 11/Programs/Week 11.1/hono-app/src/index.ts
@@ -1,9 +1,47 @@
-import { Hono } from 'hono'
+import { Hono, Next } from 'hono';
+import { Context } from 'hono/jsx';
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+// STEP 1: GET THE REQUEST
+/*
+app.get('/', async (c) => {
+  const body = await c.req.json();
+  console.log(body);
+  console.log(c.req.header('Authorization'));
+  console.log(c.req.query('param'));
 
-export default app
+  return c.text('Hello Hono!');
+});
+*/
+
+
+
+
+
+
+
+// STEP 2: APPLYING MIDDLEWARES
+async function authMiddleware(c: any, next: any) {
+  if (c.req.header('Authorization')) {
+    // Do validation
+    await next();
+  } else {
+    return c.text('You dont have acces');
+  }
+}
+
+// EITHER
+// app.use(authMiddleware);
+
+// OR
+app.post('/', authMiddleware, async (c) => {
+  const body = await c.req.parseBody();
+  console.log(body);
+  console.log(c.req.header('Authorization'));
+  console.log(c.req.query('param'));
+
+  return c.json({ msg: 'as' });
+});
+
+export default app;
