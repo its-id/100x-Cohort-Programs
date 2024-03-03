a6868402504d9aee3ffc3c51794787038f896aad
its-id <ik.dana24@gmail.com>
2024-03-03 20:08:17 +0530

    creating signup flow

--- Week 14/Week 14.2/app/api/user/route.ts
+++ Week 14/Week 14.2/app/api/user/route.ts
@@ -1,6 +1,20 @@
+import { NextRequest, NextResponse } from 'next/server';
+
 export async function GET() {
   return Response.json({
     name: 'John Doe',
     email: 'john@doe.com',
   });
+}
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+
+  return NextResponse.json(
+    {
+      username: body.username,
+      password: body.password,
+    },
+    { status: 200 }
+  );
 }
---
+++ Week 14/Week 14.2/app/signup/page.tsx
@@ -0,0 +1,9 @@
+import { Signup } from "@/components/Signup"
+
+const SignupPage = () => {
+  return (
+    <Signup />
+  )
+}
+
+export default SignupPage
