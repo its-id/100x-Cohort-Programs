1973026d3db9c35b9c08aab1404f6e228459e3cf
its-id <ik.dana24@gmail.com>
2024-03-03 19:29:03 +0530

    add server side fetching and rendering

--- Week 14/Week 14.2/app/page.tsx
+++ Week 14/Week 14.2/app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <div>Hi There!</div>;
+import axios from 'axios';
+
+async function getUserDetails() {
+  const response = await axios.get(
+    'https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details'
+  );
+  return response.data;
+}
+
+export default async function Home() {
+  const userData = await getUserDetails();
+  return (
+    <div className='h-[100vh] w-full flex flex-col justify-center items-center gap-2'>
+      <span>{userData.email}</span>
+      <span>{userData.name}</span>
+    </div>
+  );
 }
