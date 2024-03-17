670d407c58a05f265a5d396189e1dfb6123871fb
its-id <ik.dana24@gmail.com>
2024-03-17 22:09:55 +0530

    update InputComponent content

--- Week 16/Week 16.2/my-turborepo/packages/ui/src/input-component.tsx
+++ Week 16/Week 16.2/my-turborepo/packages/ui/src/input-component.tsx
@@ -1,7 +1,9 @@
 export const InputComponent = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
-      <h1>InputComponent Component</h1>
+      <h1>
+        InputComponent Component Generated using <code>turbo-generate</code>
+      </h1>
       {children}
     </div>
   );
